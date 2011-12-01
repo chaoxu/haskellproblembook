a078b980c8baf7620dd6c277b6f65808b77339c1
Mgccl <mgcclx@gmail.com>
2011-12-01 12:06:39 +0000

    Remove files

--- single.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-lhs2TeX -o $1.tex $1.lhs
-pdflatex --interaction=nonstopmode $1.tex
-bibtex $1
-pdflatex --interaction=nonstopmode $1.tex
-pdflatex --interaction=nonstopmode $1.tex
-rm $1.tex $1.aux $1.bbl $1.blg $1.log $1.out $1.ptb
